fe3d901d9143a86e48c85132892422bedc891db9
weird-frog <den.6667531@gmail.com>
2021-04-16 22:22:36 +0300

    Add project files.

---
+++ LateBindingApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace LateBindingApp
+{
+    // Эта программа загрузит внешнюю библиотеку,
+    // и создаст объект с помощью поздней привязки.
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Функция с поздним связыванием *****");
+            // Попытка загрузить локальную копию CarLibrary.
+            Assembly a = null;
+            try
+            {
+                a = Assembly.Load("CarLibrary");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            if (a != null) CreateUsingLateBinding(a);
+            Console.ReadLine();
+        }
+
+        static void CreateUsingLateBinding(Assembly asm)
+        {
+            try
+            {
+                // Получить метаданные для типа Minivan.
+                Type miniVan = asm.GetType("CarLibrary.MiniVan");
+
+                // Создание экземпляра типа Minivan динамически.
+                object obj = Activator.CreateInstance(miniVan);
+                Console.WriteLine("Created a {0} using late binding!", obj);
+                // Получение информации о TurboBoost.
+                MethodInfo mi = miniVan.GetMethod("TurboBoost");
+
+                // Вызвать метод ('null' означает, что параметров нет).
+                mi.Invoke(obj, null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void InvokeMethodWithArgsUsingLateBinding(Assembly asm)
+        {
+            try
+            {
+                // Сначала получим метаданные для типа SportsCar.
+                Type sport = asm.GetType("CarLibrary.SportsCar");
+
+                // Затем создадим экземпляр типа SportsCar динамически.
+                object obj = Activator.CreateInstance(sport);
+                // Вызов метода TurnOnRadio() с аргументами.
+                MethodInfo mi = sport.GetMethod("TurnOnRadio");
+                mi.Invoke(obj, new object[] { true, 2 });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ PR3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO; // для использования FileNotFoundException.
+namespace ExternalAssemblyReflector
+{
+    class Program
+    {
+        static void DisplayTypesInAsm(Assembly asm)
+        {
+            Console.WriteLine("\n***** Типы в сборке *****");
+            Console.WriteLine("->{0}", asm.FullName);
+            Type[] types = asm.GetTypes();
+            foreach (Type t in types) Console.WriteLine("Type: {0}", t);
+            Console.WriteLine("");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Просмотр внешней сборки *****");
+            string asmName = "";
+            Assembly asm = null;
+            do
+            {
+                Console.WriteLine("\nВведите сборку для оценки");
+                Console.Write("или Q для выхода: ");
+                // Получение имени сборки.
+                asmName = Console.ReadLine();
+                // Пользователь хочет выйти?
+                if (asmName.ToUpper() == "Q")
+                {
+                    break;
+                }
+                // Проверка загрузки сборки.
+                try
+                {
+                    asm = Assembly.Load(asmName);
+                    DisplayTypesInAsm(asm);
+                }
+                catch
+                {
+                    Console.WriteLine("Sorry, can’t find assembly.");
+                }
+            } while (true);
+        }
+    }
+}
